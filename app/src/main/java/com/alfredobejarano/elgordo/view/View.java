195e1c573e51c8d675f3402c8dbee5cefc754494
Juan Alfredo Corona Bejarano <alfredocbejarano@gmail.com>
2016-06-01 13:53:55 -0500

    Adds API call methods for the ObjectMapper

---
+++ app/src/main/java/com/alfredobejarano/elgordo/view/View.java
@@ -0,0 +1,8 @@
+package com.alfredobejarano.elgordo.view;
+
+/**
+ * Created by jacorona on 6/1/16.
+ * Activity
+ *  Interface responsable for all views behavior
+ */
+public interface View { /* Empty for now */ }
