6df56ddf8e2ac5a70535316d13eac45cf3f0a5ed
AlfredoBejarano <alfredocbejarano@gmail.com>
2017-02-08 13:12:21 -0600

    Fixes path names in packages

--- app/src/main/java/com/alfredobejarano/elgordo/network/NetworkUtils.java
+++ app/src/main/java/com/alfredobejarano/elgordo/network/NetworkUtils.java
@@ -6,7 +6,7 @@ package com.alfredobejarano.elgordo.network;
  */
 public class NetworkUtils {
     // TODO - Change the API URL for the production one (when the API enters production m8).
-    public static final String API_BASE_URL = "http://10.16.60.255:3000/";
+    public static final String API_BASE_URL = "http://192.168.0.3:3000/";
 
     /* Static flags */
     public static final int GET_ALL_DOGS = 0;
