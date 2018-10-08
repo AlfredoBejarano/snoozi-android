2e49e69b7e649ee8edb11ad7256777c28636e208
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-08 18:30:51 -0500

    Create MainActivity.kt
    Activity that will handle navigation for the app

---
+++ app/src/main/java/me/alfredobejarano/elgordo/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.alfredobejarano.elgordo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
