3ca2e6248d7eeb69bbdab9b56e18767c565bc74b
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-08 18:57:28 -0500

    Update MainActivity.kt
    Set the data binding root as the activity content view

--- app/src/main/java/me/alfredobejarano/elgordo/MainActivity.kt
+++ app/src/main/java/me/alfredobejarano/elgordo/MainActivity.kt
@@ -2,10 +2,13 @@ package me.alfredobejarano.elgordo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.alfredobejarano.elgordo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
