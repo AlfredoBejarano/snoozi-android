cfba31b8468dd07b929d8e862f668c22b11490a9
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-08 18:29:53 -0500

    Update DogPost.kt
    Add @Ignore annotation to the parameterized constructor

--- app/src/main/java/me/alfredobejarano/elgordo/data/DogPost.kt
+++ app/src/main/java/me/alfredobejarano/elgordo/data/DogPost.kt
@@ -2,6 +2,7 @@ package me.alfredobejarano.elgordo.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import java.util.*
 
@@ -23,35 +24,36 @@ class DogPost() {
     /**
      * Property that defines the title of the dog post.
      */
-    private var mTitle: String = ""
+    var title: String = ""
     /**
      * Property that holds the reference of the dog photo as a base64.
      */
-    private var mPhoto: String = ""
+    var photo: String = ""
     /**
      * Boolean flag that defines if the post is for a lost dog (true)
      * or a found dog and the  user is looking for is owner (false).
      */
-    private var mLost: Boolean = false
+    var lost: Boolean = false
     /**
      * Property that contains more info about the dog, things like the breed
      * its size, where it was lost/found or the poster phone number.
      */
-    private var mDescription: String = ""
+    var description: String = ""
     /**
      * Value that contains when this post was created.
      */
     @ColumnInfo(name = "created")
-    val createdAt = Calendar.getInstance().time?.time
+    var createdAt = Calendar.getInstance().time?.time
 
     /**
      * Constructor that allows creating a dog
      * object with a title, photo and a description.
      */
+    @Ignore
     constructor(title: String, photo: String, description: String, lost: Boolean) : this() {
-        mLost = lost
-        mTitle = title
-        mPhoto = photo
-        mDescription = description
+        this.lost = lost
+        this.title = title
+        this.photo = photo
+        this.description = description
     }
 }
